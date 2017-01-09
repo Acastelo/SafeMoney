1167a2a4a017caef25ea31c7fe9d89ec851ae5a3
Luiz Alexandre <luiz.alexandre@projeto.lead>
2017-01-09 17:53:34 -0300

    Inclusão de pageview na MainaActivity

--- app/src/main/java/com/ags/safemoney/activities/MainActivity.java
+++ app/src/main/java/com/ags/safemoney/activities/MainActivity.java
@@ -1,15 +1,60 @@
 package com.ags.safemoney.activities;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ListFragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ags.safemoney.R;
+import com.ags.safemoney.activities.fragment.ListaFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentPagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vpPager);
+        adapterViewPager = new MeuAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapterViewPager);
+    }
+
+    public static class MeuAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITENS = 4;
+
+        public MeuAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITENS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return ListaFragment.newInstance(0, "PAGINA #1");
+                case 1:
+                    return ListaFragment.newInstance(1, "PAGINA #2");
+                case 2:
+                    return ListaFragment.newInstance(2, "PAGINA #3");
+                case 3:
+                    return ListaFragment.newInstance(3, "PAGINA #4");
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "PAGINA " + position;
+        }
     }
 }
---
+++ app/src/main/java/com/ags/safemoney/activities/fragment/ListaFragment.java
@@ -0,0 +1,45 @@
+package com.ags.safemoney.activities.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.ags.safemoney.R;
+
+/**
+ * Created by luiz.alexandre on 09/01/2017.
+ */
+
+public class ListaFragment extends Fragment {
+
+    private String title;
+    private int page;
+
+    public static ListaFragment newInstance(int page, String title){
+        ListaFragment listaFragment = new ListaFragment();
+        Bundle args = new Bundle();
+        args.putInt("pagina", page);
+        args.putString("titulo", title);
+        listaFragment.setArguments(args);
+
+        return listaFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("pagina", 0);
+        title = getArguments().getString("titulo");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.lista_fragment, container, false);
+        TextView label = (TextView) view.findViewById(R.id.tv_label);
+        label.setText(page + " -- " + title);
+        return view;
+    }
+}
